cf2a0453055d80bec157b920d96a0a699d7776ca
Alexis BRIET <alexis.briet2003@gmail.com>
2023-12-05 10:59:33 +0100

    Update create post

--- src/main/java/com/findme/api/model/dto/PostDTO.java
+++ src/main/java/com/findme/api/model/dto/PostDTO.java
@@ -1,6 +1,8 @@
 package com.findme.api.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.findme.api.model.Post;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -13,18 +15,22 @@ import java.util.Map;
 public class PostDTO {
 	@Id
 	private String id;
-	
+
+	@NotNull
 	private String userId;
 	
+	@NotNull
 	private Map<String,String> picture;
 	
 	private String description;
-	
+
+	@NotNull
 	private Post.Geolocation geolocation;
 	
 	private List<Post.View> view;
 	
 	private List<Post.Like> like;
 	
+	@JsonIgnore
 	private boolean verified;
 }
